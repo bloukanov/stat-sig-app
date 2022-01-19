8846f186fa867bedae4e290b19e9a9b990b2096f
Bogdan <loukanob@mskcc.org>
2022-01-19 17:06:14 -0500

    finished evaluate a test section

--- significance_test_app.py
+++ significance_test_app.py
@@ -5,7 +5,13 @@ import streamlit as st
 
 # session_seed = 1
 
-acknowledgers = ['Ok','Sounds good','Perfect','Excellent','Great','Alrighty']#, Fantastic
+# ---------------------
+#   HELPER FUNCTIONS
+# ---------------------
+
+# acknowledgers
+# ---------------
+acknowledgers = ['Ok','Sounds good','Perfect','Great','Alrighty', 'Got it']#, Fantastic 'Excellent',
 neg_acknowledgers = ["That's ok", "That's alright", "No problem"]
 exclamations = ['!','.']
 
@@ -15,12 +21,74 @@ def generate_acks(m,n):
     acks = []
     neg_acks = []
     for i in range(m):
-        acks.append(acknowledgers[np.random.randint(0,len(acknowledgers))] + exclamations[np.random.randint(0,2)])
+        acks.append(acknowledgers[np.random.randint(0,len(acknowledgers))] + exclamations[np.random.randint(0,len(exclamations))])
     for i in range(n):
-        neg_acks.append(neg_acknowledgers[np.random.randint(0,len(neg_acknowledgers))] + exclamations[np.random.randint(0,2)])
+        neg_acks.append(neg_acknowledgers[np.random.randint(0,len(neg_acknowledgers))] + exclamations[np.random.randint(0,len(exclamations))])
     return acks , neg_acks
 
 acks = generate_acks(10,10)
+
+
+# conduct independent samples t-test
+# ------------------------------------
+def custom_ttest(_group1,_group2,test_type,_0s_desired=None,_0s_included=None,n1=None,n2=None):
+
+    if test_type == 'ind':
+
+        yes_no_bool = {'Yes':True,'No':False}
+
+        _0s_desired = yes_no_bool[_0s_desired]
+        _0s_included = yes_no_bool[_0s_included]
+
+        if (_0s_desired and _0s_included) or not (_0s_desired or _0s_included):
+            group1 = _group1
+            group2 = _group2
+
+        elif _0s_desired and not _0s_included:
+            group1 = _group1.append(pd.Series(np.repeat(0,n1-len(_group1))))
+            group2 = _group2.append(pd.Series(np.repeat(0,n2-len(_group2))))
+
+        elif (not _0s_desired) and _0s_included:
+            group1 = _group1[_group1 != 0]
+            group2 = _group2[_group2 != 0]
+
+        # check for equality of variance. levene seems to not be sensitive to assumptions of normality.
+        var_test = levene(group1[~pd.isna(group1)],group2[~pd.isna(group2)])
+        # print(var_test[1])
+        # if p val is not significant at 10% level, assume variances are equal
+        if var_test[1] >= .1:
+            result = ttest_ind(group1,group2,nan_policy='omit',equal_var=True)
+        # otherwise, can assume they are unequal
+        elif var_test[1] < .1:
+            result = ttest_ind(group1,group2,nan_policy='omit',equal_var=False)
+
+    elif test_type == 'rel':
+            group1 = _group1
+            group2 = _group2
+            result = ttest_rel(group1,group2,nan_policy='omit')
+
+    # print(len(group1))
+    # print(len(group2))
+
+    pval = result[1]
+    mean1 = group1.mean()
+    mean2 = group2.mean()
+    st.write('Mean 1: {:.2f}. Mean 2: {:.2f}. Mean difference: {:.2f}'.format(mean1,mean2,mean1-mean2))
+    st.write('P-Value: '+'{:.3f}'.format(pval))
+    if pval < .01:
+        st.success('This difference is significant at the 1% level.')
+    elif pval < .05:
+        st.success('This difference is significant at the 5% level.')
+    elif pval < .1:
+        st.success('This difference is significant at the 10% level.')
+    else:
+        st.warning('This difference would not typically be considered statistically significant.')
+
+
+
+#-----------------------------------------------------
+#-----------------------------------------------------
+
 
 st.title('Statistical Significance Workbook')
 # st.header('Brought to you by the Decision Science team')
@@ -70,63 +138,62 @@ if plan_eval == 'Evaluate a test':
                     st.warning('This difference would not typically be considered statistically significant.')
 
     elif means_rates == 'Difference in means':
-        rev_per_sess = st.sidebar.selectbox('Consider all sessions?',['Select one','Yes','No'])
-        st.write(acks[0][2] + ''' Would you like to account for emails or sessions that did not
-        result in a gift? This metric would be revenue per session, as opposed to average gift.
-        INCLUDE LINKS''')
-        if rev_per_sess != 'Select one':
-            st.write(acks[0][3] + ''' Does your data have 0's to represent sessions without a transaction?''')
-            _0s_in_data = st.sidebar.selectbox('Data for every session?',['Select one','Yes','No'])
-        if (rev_per_sess == 'Yes' and _0s_in_data == 'Yes') or (rev_per_sess == 'No' and _0s_in_data == 'No'):
-            st.write(acks[0][4] + ''' Are the 2 groups independent of one another? This will usually be the case,
-            unless you are comparing, say, 2 different treatments given to the exact same people.''')
-            ind = st.sidebar.selectbox('Independent samples?', ['Select one','Yes','No'])  
-            # print(ind)  
-            if ind == 'Yes':
-                st.write(acks[0][5] + ''' Upload a csv with your data in columns named 'Group1' and 'Group2.'
-                ''')
-                with st.form('submit mean inputs 1'):
-                    upload = st.file_uploader('Upload data', type='csv')
-                    means1submit = st.form_submit_button()
-                    if means1submit:
-                        df = pd.read_csv(upload)
-                        # check for equality of variance. levene seems to not be sensitive to assumptions of normality.
-                        var_test = levene(df.Group1[~pd.isna(df.Group1)],df.Group2[~pd.isna(df.Group2)])
-                        print(var_test[1])
-                        # if p val is not significant at 10% level, assume variances are unequal
-                        if var_test[1] > .1:
-                            result = ttest_ind(df.Group1,df.Group2,nan_policy='omit',equal_var=False)
-                        # otherwise, can assume they are equal
-                        else:
-                            result = ttest_ind(df.Group1,df.Group2,nan_policy='omit',equal_var=True)
-                        pval = result[1]
-                        mean1 = df.Group1.mean()
-                        mean2 = df.Group2.mean()
-                        st.write('Mean 1: {:.2f}. Mean 2: {:.4f}. Mean difference: {:.2f}'.format(mean1,mean2,mean1-mean2))
-                        st.write('P-Value: '+'{:.3f}'.format(pval))
-                        if pval < .01:
-                            st.success('This difference is significant at the 1% level.')
-                        elif pval < .05:
-                            st.success('This difference is significant at the 5% level.')
-                        elif pval < .1:
-                            st.success('This difference is significant at the 10% level.')
-                        else:
-                            st.warning('This difference would not typically be considered statistically significant.')
-
-
-            # st.write(acks[0][5] + ''' Upload a csv with your data in columns named 'Group1' and 'Group2.'
-            # ''')
-            # with st.form('submit mean inputs 2'):
-            #     st.file_uploader('Upload data', type='csv')
-            #     means1submit = st.form_submit_button()
-            #     if means1submit:
-            #         pass
-
-        elif (rev_per_sess == 'Yes' and _0s_in_data == 'No'):
-            st.write('''No problem! I can adjust that for you. Upload a csv with your data in 
-            columns named 'Group1' and 'Group2.'
+        st.write(acks[0][2] + ''' Are the 2 groups independent of one another? (This will usually be the case,
+        unless you are comparing, say, 2 different treatments given to the exact same people.)''')
+        ind = st.sidebar.selectbox('Independent samples?', ['Select one','Yes','No'])  
+        # print(ind)  
+        if ind == 'Yes':
+            st.write(acks[0][3] + ''' We'll conduct an [independent samples t-test] (https://en.wikipedia.org/wiki/Student%27s_t-test#Independent_(unpaired)_samples). 
+            Would you like to account for emails or sessions that did not
+            result in a gift? This metric would be revenue per visitor or session, as opposed to average gift.
+            You can read more about the relationship between conversion rate, average gift, and revenue per visitor [here] (https://vwo.com/blog/important-ecommerce-metrics/).
             ''')
-        elif (rev_per_sess == 'No' and _0s_in_data == 'Yes'):
-            st.write('''No problem! I can adjust that for you. Upload a csv with your data in 
-            columns named 'Group1' and 'Group2.'
+            rev_per_sess = st.sidebar.selectbox('Consider all sessions?',['Select one','Yes','No'])
+            if rev_per_sess != 'Select one':
+                st.write(acks[0][4] + ''' Does your data have 0's to represent sessions without a transaction?''')
+                _0s_in_data = st.sidebar.selectbox('Data for every session?',['Select one','Yes','No'])
+
+                if _0s_in_data != 'Select one':
+
+                    if (rev_per_sess == 'Yes' and _0s_in_data == 'Yes') or (rev_per_sess == 'No' and _0s_in_data == 'No'):
+                        st.write(acks[0][5] + ''' Upload a csv with your data in columns named 'Group1' and 'Group2,' and click Submit.
+                        ''')
+                        total_obs1 = None
+                        total_obs2 = None
+
+                    elif (rev_per_sess == 'Yes' and _0s_in_data == 'No'):
+                        st.write(acks[1][0] + ''' I can adjust that for you. Enter the total number of observations for each group
+                        (including those without a transaction), and then upload a csv with your data in columns named 'Group1' and 'Group2.'
+                        Then click Submit.
+                        ''')
+                        col3, col4 = st.columns(2)
+                        total_obs1 = col3.number_input('Total observations for Group 1',1)
+                        total_obs2 = col4.number_input('Total observations for Group 2',1)
+                    elif (rev_per_sess == 'No' and _0s_in_data == 'Yes'):
+                        st.write(acks[1][1] + ''' I'll remove the 0's for you. Upload a csv with your data in 
+                        columns named 'Group1' and 'Group2,' and click Submit.
+                        ''')
+                        total_obs1 = None
+                        total_obs2 = None
+
+                    with st.form('submit mean inputs 1'):
+                        upload = st.file_uploader('Upload data', type='csv')
+                        means1submit = st.form_submit_button()
+                        if means1submit:
+                            df = pd.read_csv(upload)
+                            custom_ttest(df.Group1,df.Group2,'ind',rev_per_sess,_0s_in_data,n1=total_obs1,n2=total_obs2)
+                        
+        elif ind == 'No':
+            st.write(acks[0][6] + ''' We'll conduct a [paired samples t-test] (https://en.wikipedia.org/wiki/Student%27s_t-test#Paired_samples).
+            Upload a csv with your data in columns named 'Group1' and 'Group2,' and click Submit. 
+            Note that both groups must have the same number of observations.
             ''')
+            with st.form('submit mean inputs 2'):
+                upload = st.file_uploader('Upload data', type='csv')
+                means2submit = st.form_submit_button()
+                if means2submit:
+                    df = pd.read_csv(upload)
+                    custom_ttest(df.Group1,df.Group2,'rel')
+
+elif plan_eval == 'Plan for a test':
+    st.write('Under construction!')
